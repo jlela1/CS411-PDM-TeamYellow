edc37c3c03ec9b001387a7aea10ad2e253f4dd06
MariDemissiew <106851438+MariDemissiew@users.noreply.github.com>
2023-09-05 15:37:49 -0400

    Create helloworld.java

---
+++ src/backend/helloworld.java
@@ -0,0 +1,9 @@
+public class HelloWorldBackend {
+    public static String getHelloMessage() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getHelloMessage());
+    }
+}
