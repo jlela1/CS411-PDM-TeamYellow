ebca683bb62720305d6391eebd649dffe15f4dda
eturkogl <eturk004@odu.edu>
2023-09-05 20:42:58 -0400

    Created frontend baseline

---
+++ src/frontend/HelloWorldFrontend.java
@@ -0,0 +1,11 @@
+package frontend;
+
+public class HelloWorldFrontend {
+    public static String getHelloMessage() {
+         return "Hello from Frontend!";
+    }
+    
+public static void main (String[] args) {
+    System.out.println(getHelloMessage());
+    }
+}
