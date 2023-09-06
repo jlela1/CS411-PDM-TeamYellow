c2014570ef0cbf52dde28beb79eeb2784c8201e1
jleland <jlela002@odu.edu>
2023-09-06 14:10:09 -0400

    Added test instance for testTable - WIP

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
 
 import frontend.HelloWorldFrontend;
+import backend.database.testTable;
 public class Main {
     public static void main(String[] args) {
 
         //Execute backend to frontend data transfer/output
         HelloWorldFrontend helloOutput = new HelloWorldFrontend();
         helloOutput.outputHelloWorld();
+
+        //Create test instance of database:
+        testTable table = new testTable(123, "John", "Doe", "test", 1, "test", true);
+        System.out.println("Table Test: " + table.user_firstname + " " + table.user_lastname + " " + table.vehicle_id); // needs work
+
 
     }
 }
