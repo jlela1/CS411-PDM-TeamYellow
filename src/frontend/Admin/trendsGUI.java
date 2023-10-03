8391b3e4893aa3152d8c1aef2061b136c9e4eac6
bmcca004 <bmcca004@odu.edu>
2023-10-03 04:50:16 -0400

    Redesign.

--- src/frontend/Admin/trendsGUI.java
+++ src/frontend/Admin/trendsGUI.java
@@ -2,84 +2,90 @@ package frontend.Admin;
 
 import javax.swing.*;
 import java.awt.*;
+
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class trendsGUI extends JFrame {
-    /* private JTextField headerParkingDemandManagementTextField;
-     private JPanel trendsPanel;
-     private JButton button1;
-     public JList garageSelection;
 
+public class trendsGUI extends JFrame implements ActionListener  {
+    JComboBox timeSelection;
+    JLabel headerLabel, footerLabel ;
+    JPanel headerPanel,subPanel;
 
-
-     public trendsGUI() {
-         String gNames[]= {"Garage1","Garage2","Garage3"};
-         JList garageSelection = new JList(gNames);
-
-         */
     public trendsGUI() {
         setTitle("PDM Trends Dashboard");
-        setSize(600, 600);
+        setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JPanel headerPanel = new JPanel();
-        headerPanel.setBackground(new Color(0, 0, 0, 128));
+         headerPanel = new JPanel();
+        subPanel = new JPanel();
+
         headerPanel.setLayout(new BorderLayout());
 
-        JLabel headerLabel = new JLabel("PDM Trends Dashboard:");
+        headerLabel = new JLabel("Welcome to the PDM Trends Dashboard:");
 
         headerLabel.setFont(new Font("Verdana", Font.BOLD, 32));
-        headerLabel.setForeground(Color.WHITE);
+        headerLabel.setForeground(Color.BLUE);
         headerLabel.setHorizontalAlignment(JLabel.CENTER);
 
 
+         footerLabel = new JLabel("PDM Trends Dashboard");
+        footerLabel.setBackground(new Color(0,0,0,128));
+        footerLabel.setFont(new Font("Verdana", Font.BOLD, 32));
+        footerLabel.setForeground(Color.BLUE);
+        footerLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        add(headerLabel,BorderLayout.NORTH);
+        add(footerLabel,BorderLayout.SOUTH);
 
 
-        JLabel promptText = new JLabel("<html>Welcome to the Parking Demand Management Trends Dashboard<br/>Please select which garage you wish to view</html>");
-        promptText.setHorizontalAlignment(JLabel.CENTER);
+        String times[] = {"00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30",
+        "06:00","06:30","07:00","07:30","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30",
+                "13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30"
+        ,"20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30"}; //48 elements for timeSelection
 
-        add(headerPanel, BorderLayout.CENTER);
-        add(promptText,BorderLayout.NORTH);
 
-        JButton garage1 = new JButton("Garage1");
-        garage1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"G1 Place Holder");
 
-            }
-        });
-        headerPanel.add(garage1, BorderLayout.WEST);
-        JButton garage2 = new JButton("Garage2");
-        garage2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"G2 Place Holder");
 
-            }
-        });
-        headerPanel.add(garage2, BorderLayout.CENTER);
-        JButton garage3 = new JButton("Garage3");
-        garage3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(null,"G3 Place Holder");
 
-            }
-        });
-        headerPanel.add(garage3, BorderLayout.EAST);
+         timeSelection = new JComboBox<>(times);
+        timeSelection.setBounds(50,50,90,20);
 
-        JLabel footerPanel = new JLabel("PDM");
-        footerPanel.setHorizontalAlignment(JLabel.CENTER);
-        add(footerPanel, BorderLayout.SOUTH);
+
+        JLabel timeText = new JLabel("Please select the time you wish to view:");
+        subPanel.add(timeText);
+        timeSelection.addActionListener(this);
+
+        subPanel.add(timeSelection);
+
+        add(subPanel,BorderLayout.CENTER);
 
 
 
 
 
 
+
+
+
+
+
+
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==timeSelection)
+        {
+            System.out.println(timeSelection.getSelectedIndex());
+
+
+
+
+        }
 
     }
 
