f2ae4f33f362444f3ea2582ff5257ce31a2c1c91
Heather <hmars005@odu.edu>
2023-10-03 15:06:03 -0400

    Generates the new popup page for the login form

---
+++ src/frontend/Page.java
@@ -0,0 +1,16 @@
+package frontend;
+
+import javax.swing.*;
+import java.awt.*;
+class NewPage extends JFrame{
+
+    NewPage()
+    {
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Welcome");
+        setSize(400, 200);
+
+
+    }
+}
+
