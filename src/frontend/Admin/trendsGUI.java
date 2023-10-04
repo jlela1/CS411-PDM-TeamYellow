d640e4558ac42ab8c2caaacf971c55caa8661fb7
Ece Turkoglu <eturk004@odu.edu>
2023-10-04 00:54:43 -0400

    Trends changes -ece

--- src/frontend/Admin/trendsGUI.java
+++ src/frontend/Admin/trendsGUI.java
@@ -2,103 +2,146 @@ package frontend.Admin;
 
 import javax.swing.*;
 import java.awt.*;
-
-
+import java.util.HashMap;
+import java.util.Map;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class trendsGUI extends JFrame implements ActionListener  {
-    JComboBox timeSelection;
-    JLabel headerLabel, footerLabel ;
-    JPanel headerPanel,subPanel;
+public class trendsGUI extends JFrame  {
+    private JComboBox<String> timeSelection;
+    private JButton homeButton;
+    private JPanel garagePanel;
+    private Map<String, Integer> garageCapacities;
+    //JLabel headerLabel, footerLabel ;
+    //JPanel headerPanel,subPanel;
 
     public trendsGUI() {
         setTitle("PDM Trends Dashboard");
-        setSize(800, 600);
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // Set JFrame to full-screen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-         headerPanel = new JPanel();
-        subPanel = new JPanel();
-
+        initializeGarageCapacities();
+// Header
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(Color.GRAY);
         headerPanel.setLayout(new BorderLayout());
 
-        headerLabel = new JLabel("Welcome to the PDM Trends Dashboard:");
+        JLabel headingLabel = new JLabel("PDM Trends Page");
+        headingLabel.setFont(new Font("Roboto", Font.BOLD, 32));
+        headingLabel.setForeground(Color.WHITE);
+        headingLabel.setHorizontalAlignment(JLabel.CENTER);
+        headerPanel.add(headingLabel, BorderLayout.NORTH);
 
-        headerLabel.setFont(new Font("Verdana", Font.BOLD, 32));
-        headerLabel.setForeground(Color.BLUE);
-        headerLabel.setHorizontalAlignment(JLabel.CENTER);
+        // Time Dropdown
+        JPanel timeSection = new JPanel();
+        timeSection.setBackground(Color.GRAY);
+        timeSelection = new JComboBox<>(new String[]{
+                "07:00 AM", "07:10 AM", "07:20 AM", "07:30 AM", "07:40 AM", "07:50 AM",
+                "08:00 AM", "08:10 AM", "08:20 AM", "08:30 AM", "08:40 AM", "08:50 AM",
+                "09:00 AM", "09:10 AM", "09:20 AM", "09:30 AM", "09:40 AM", "09:50 AM",
+                "10:00 AM"
+        });
+        timeSelection.setFont(new Font("Roboto", Font.PLAIN, 16));
+        timeSelection.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateGarageCapacities();
+            }
+        });
+        timeSection.add(new JLabel("Select Time Period:"));
+        timeSection.add(timeSelection);
+        headerPanel.add(timeSection, BorderLayout.CENTER);
+        add(headerPanel, BorderLayout.NORTH);
 
+        // Garage Panel
+        garagePanel = new JPanel();
+        garagePanel.setLayout(new GridLayout(3, 1));
 
-         footerLabel = new JLabel("PDM Trends Dashboard");
-        footerLabel.setBackground(new Color(0,0,0,128));
-        footerLabel.setFont(new Font("Verdana", Font.BOLD, 32));
-        footerLabel.setForeground(Color.BLUE);
+        // Add initial garage information to the panel
+        updateGarageCapacities();
+
+        JScrollPane scrollPane = new JScrollPane(garagePanel);
+        add(scrollPane, BorderLayout.CENTER);
+
+        // Home Button
+        JButton controlsPanel = new JButton();
+        homeButton = new JButton(" Go Home Page");
+        homeButton.setFont(new Font("Roboto", Font.BOLD, 15));
+        controlsPanel.add(homeButton);
+        controlsPanel.setFocusPainted(false);
+        add(controlsPanel, BorderLayout.LINE_START);
+
+        JLabel footerLabel = new JLabel("Parking Demand Management (PDM)");
+        footerLabel.setFont(new Font("Roboto", Font.ITALIC, 15));
+        footerLabel.setForeground(Color.WHITE);
         footerLabel.setHorizontalAlignment(JLabel.CENTER);
 
-        add(headerLabel,BorderLayout.NORTH);
-        add(footerLabel,BorderLayout.SOUTH);
-
-
-        String times[] = {"00:00","00:30","01:00","01:30","02:00","02:30","03:00","03:30","04:00","04:30","05:00","05:30",
-        "06:00","06:30","07:00","07:30","08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30","12:00","12:30",
-                "13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30"
-        ,"20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30"}; //48 elements for timeSelection
-
-
-
-
-
-         timeSelection = new JComboBox<>(times);
-        timeSelection.setBounds(50,50,90,20);
-
-
-        JLabel timeText = new JLabel("Please select the time you wish to view:");
-        subPanel.add(timeText);
-        timeSelection.addActionListener(this);
-
-        subPanel.add(timeSelection);
-
-        add(subPanel,BorderLayout.CENTER);
-
-
-
-
-
-
-
-
-
-
-
-
+        JPanel footerPanel = new JPanel();
+        footerPanel.setBackground(new Color(122, 114, 114, 173));
+        footerPanel.add(footerLabel);
+        add(footerPanel, BorderLayout.SOUTH);
 
     }
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource()==timeSelection)
-        {
-            System.out.println(timeSelection.getSelectedIndex());
+
+    private void initializeGarageCapacities() {
+        garageCapacities = new HashMap<>();
+        garageCapacities.put("  Football Stadium", 44);
+        garageCapacities.put("  43rd & Elkhorn", 55);
+        garageCapacities.put("  43rd & Bluestone", 49);
+    }
+
+    private void updateGarageCapacities() {
+        String selectedTime = (String) timeSelection.getSelectedItem();
+
+        // Check if the selectedTime string has a valid format (hh:mm AM/PM)
+        if (selectedTime.matches("\\d{2}:\\d{2} [APap][Mm]")) {
+            // Extract the hour part from the selected time
+            int hour = Integer.parseInt(selectedTime.substring(0, 2));
+
+            // Simulate changing capacities based on the selected time
+            int capacityChange = hour >= 7 && hour <= 10 ? hour - 6 : 0;
+
+            // Update garage capacities
+            for (Map.Entry<String, Integer> entry : garageCapacities.entrySet()) {
+                int updatedCapacity = entry.getValue() + capacityChange;
+                entry.setValue(Math.max(updatedCapacity, 0));
+            }
 
 
+            updateGaragePanel();
+        } else {
+            // Handle invalid input format
+            JOptionPane.showMessageDialog(this, "Invalid time format. Please select a valid time.");
+        }
+    }
 
 
+    private void updateGaragePanel() {
+        garagePanel.removeAll();
+
+        for (Map.Entry<String, Integer> entry : garageCapacities.entrySet()) {
+            JPanel garageInfoPanel = new JPanel(new GridLayout(3, 1));
+            garageInfoPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+            JLabel garageNameLabel = new JLabel( entry.getKey());
+            JLabel capacityLabel = new JLabel("  Capacity: " + entry.getValue());
+
+            garageInfoPanel.add(garageNameLabel);
+            garageInfoPanel.add(capacityLabel);
+
+            garagePanel.add(garageInfoPanel);
         }
 
+        garagePanel.revalidate();
+        garagePanel.repaint();
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                trendsGUI trends = new trendsGUI();
-
-                trends.setVisible(true);
-            }
+        SwingUtilities.invokeLater(() -> {
+            trendsGUI trendsPage = new trendsGUI();
+            trendsPage.setVisible(true);
         });
     }
-
 }
-
