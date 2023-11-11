20e115ca56a1ff75f9b8e869858c1ab27ecb11b3
bmcca004 <bmcca004@odu.edu>
2023-11-11 17:08:11 -0500

    businessFeedback --- in progress

---
+++ src/frontend/businessFeedback.java
@@ -0,0 +1,157 @@
+package frontend;
+import javax.swing.*;
+
+import backend.database.Garage;
+import backend.database.numVehEnteringRate;
+import com.github.lgooddatepicker.components.DatePicker;
+import frontend.createGraph;
+import frontend.GarageManager;
+import java.awt.*;
+
+
+
+import java.time.*;
+import java.util.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class businessFeedback extends JFrame implements  ActionListener{
+    private JButton getGraph, home;
+    private String garageName, graphType;
+    private JComboBox userSelectionGarage;
+    private LocalDate date1, date2 ;
+    private DatePicker datePickerStart, datePickerEnd;
+    private JPanel graphPanel;
+
+    private int numGar;
+
+    static String garageFeedback = "Feedback";
+    createGraph feedbackGraph;
+
+
+
+
+
+
+
+public businessFeedback(String garageName, int numGarages, ArrayList<Garage> garages)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+//action listener for buttons
+
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==getGraph)
+        {
+            if (graphPanel != null) {
+                graphPanel.remove(feedbackGraph.getContentPane());
+            }
+
+            this.revalidate();
+            this.repaint();
+
+            graphType = garageFeedback;
+            String garageNewName = (String) userSelectionGarage.getSelectedItem();
+
+            feedbackGraph = new createGraph(garageNewName, numGar, datePickerStart.getDate(), datePickerEnd.getDate(),graphType);
+            date1 = datePickerStart.getDate();
+            date2 = datePickerEnd.getDate();
+            graphPanel.add(feedbackGraph.getContentPane());
+
+            this.revalidate();
+            this.repaint();
+
+
+
+        } else if (e.getSource()==home) //home button listener
+        {
+            dispose();
+            AdminHomePage adminHomePage = new AdminHomePage();
+            adminHomePage.setVisible(true);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args)
+    {
+        ArrayList<Garage> garages = new ArrayList<Garage>();
+
+        Garage garage1 = new Garage("43rd & Elkhorn Ave", 655);
+        garage1.setGarageID(0);
+        garage1.setAvgParkingDuration(180);
+        garage1.setNumVehiclesEnteringPerMin(1); //starts at 1 car per min at 7am
+        garage1.setAvgTimeToPark(10);
+        //set variable vehicles per minute
+        garage1.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
+        garage1.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
+        garage1.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
+        garage1.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
+        garage1.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
+        garage1.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
+
+        Garage garage2 = new Garage("Constant Center South", 1535);
+        garage2.setGarageID(1);
+        garage2.setAvgParkingDuration(180);
+        garage2.setNumVehiclesEnteringPerMin(1);
+        garage2.setAvgTimeToPark(10);
+        //set variable vehicles per minute
+        garage2.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
+        garage2.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
+        garage2.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
+        garage2.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
+        garage2.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
+        garage2.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
+
+        Garage garage3 = new Garage("Constant Center North", 1045);
+        garage3.setGarageID(2);
+        garage3.setAvgParkingDuration(180);
+        garage3.setNumVehiclesEnteringPerMin(1);
+        garage3.setAvgTimeToPark(10);
+        //set variable vehicles per minute
+        garage3.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
+        garage3.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
+        garage3.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
+        garage3.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
+        garage3.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
+        garage3.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
+
+        Garage garage4 = new Garage("49th Street Stadium", 745);
+        garage4.setGarageID(3);
+        garage4.setAvgParkingDuration(180);
+        garage4.setNumVehiclesEnteringPerMin(1);
+        garage4.setAvgTimeToPark(10);
+        //set variable vehicles per minute
+        garage4.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
+        garage4.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
+        garage4.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
+        garage4.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
+        garage4.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
+        garage4.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
+
+        garages.add(0, garage1);
+        garages.add(1, garage2);
+        garages.add(2, garage3);
+        garages.add(3, garage4);
+
+        businessFeedback businessFeedback = new businessFeedback("43rd & Elkhorn Ave", 4, garages);
+    }
+}
