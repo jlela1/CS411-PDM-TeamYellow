e55460bc2634658510b0dc16bc05d3d7f041fc09
bmcca004 <bmcca004@odu.edu>
2023-11-15 11:40:36 -0500

    Fixed Dropdown Menus businessFeedback and trendsGUI

--- src/frontend/businessFeedback.java
+++ src/frontend/businessFeedback.java
@@ -66,6 +66,7 @@ public businessFeedback(String garageName, int numGarages, ArrayList<Garage> gar
     for (Garage garage : garages) {
         userSelectionGarage.addItem(garage.getName());
     }
+    userSelectionGarage.addItem("All Garages");
     gbc.gridy=1;
     gbc.gridx=1;
     businessFeedBackGComponents.add(userSelectionGarage,gbc);
@@ -260,19 +261,12 @@ public businessFeedback(String garageName, int numGarages, ArrayList<Garage> gar
         garage4.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
         garage4.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
 
-        Garage garage5 = new Garage("All Garages", 745);
-        garage5.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
-        garage5.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
-        garage5.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
-        garage5.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
-        garage5.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
-        garage5.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
         garages.add(0, garage1);
         garages.add(1, garage2);
         garages.add(2, garage3);
         garages.add(3, garage4);
-        garages.add(4,garage5);
 
-        businessFeedback businessFeedback = new businessFeedback("43rd & Elkhorn Ave", 5, garages);
+
+        businessFeedback businessFeedback = new businessFeedback("43rd & Elkhorn Ave", 4, garages);
     }
 }
--- src/frontend/trendsGUI.java
+++ src/frontend/trendsGUI.java
@@ -82,7 +82,9 @@ public class trendsGUI extends JFrame implements ActionListener {
         // Add items to the combo box
         for (Garage garage : garages) {
             userSelectionGarage.addItem(garage.getName());
+
         }
+        userSelectionGarage.addItem(("All Garages"));
         // add combobox to gridbag
         gbc.gridy= 1;
         gbc.gridx= 0;
@@ -278,13 +280,6 @@ public class trendsGUI extends JFrame implements ActionListener {
         garage4.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
         garage4.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
 
-        Garage garage5 = new Garage("All Garages", 745);
-        garage5.variableNumVehPerMin.add(0, new numVehEnteringRate(480, 3));//change number of vehicles entering per minute at 480 min (8am) to 3
-        garage5.variableNumVehPerMin.add(1, new numVehEnteringRate(600, 5)); //change number of vehicles entering per minute at 600 min (10am) to 5
-        garage5.variableNumVehPerMin.add(2, new numVehEnteringRate(720, 7)); //change number of vehicles entering per minute at 600 min (12pm) to 8
-        garage5.variableNumVehPerMin.add(3, new numVehEnteringRate(840, 5)); //change number of vehicles entering per minute at 840 min (2pm) to 5
-        garage5.variableNumVehPerMin.add(4, new numVehEnteringRate(960, 3)); //change number of vehicles entering per minute at 960 min (4pm) to 3
-        garage5.variableNumVehPerMin.add(5, new numVehEnteringRate(1080, 1)); //change number of vehicles entering per minute at 1080 min (6pm) to 1
 
         garage4.setGarageID(3);
         garage4.setAvgParkingDuration(180);
@@ -294,9 +289,9 @@ public class trendsGUI extends JFrame implements ActionListener {
         garages.add(1, garage2);
         garages.add(2, garage3);
         garages.add(3, garage4);
-        garages.add(4,garage5);
 
-        trendsGUI trendsGUI = new trendsGUI("43rd & Elkhorn Ave", 5, garages);
+
+        trendsGUI trendsGUI = new trendsGUI("43rd & Elkhorn Ave", 4, garages);
     }
 
 
