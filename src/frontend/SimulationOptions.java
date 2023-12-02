d09d0efcf838fd861970074c1a6d21f993f3bdf9
Heather <hmars005@odu.edu>
2023-12-02 18:25:45 -0500

    Added labels and changed background

--- src/frontend/SimulationOptions.java
+++ src/frontend/SimulationOptions.java
@@ -20,17 +20,33 @@ public class SimulationOptions extends JFrame {
         setTitle("PDM Simulation Options");
         setExtendedState(JFrame.MAXIMIZED_BOTH); // Set JFrame to full-screen
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+
+
+        JPanel backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon backgroundImage = new ImageIcon ("resources/Background 6.png");
+                g.drawImage(backgroundImage.getImage(),0,0,getWidth(),getHeight(),this);
+            }
+        };
+
+        setContentPane(backgroundPanel);
+        backgroundPanel.setLayout(new BorderLayout());
 
         // Header Panel
         JPanel header = PDMPanels.createHeader("PDM Simulation Options");
-        add(header, BorderLayout.NORTH);
+
+
 
         header.add(Box.createRigidArea(new Dimension(0, 20))); // Add spacing
         add(header, BorderLayout.NORTH);
+        backgroundPanel.add(header, BorderLayout.NORTH);
 
         JPanel componentsPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
+        componentsPanel.setOpaque(false);
+        backgroundPanel.add(componentsPanel, BorderLayout.CENTER);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
@@ -42,7 +58,7 @@ public class SimulationOptions extends JFrame {
         JButton CustomSimulationButton = new JButton("Custom Simulation");
         CustomSimulationButton.setFont(new Font("Monospaced", Font.BOLD, 16));
         CustomSimulationButton.setForeground(Color.BLACK);
-        CustomSimulationButton.setBackground(new Color(98, 139, 145, 250));
+        CustomSimulationButton.setBackground(new Color(164, 196, 201, 250));
         CustomSimulationButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
         CustomSimulationButton.setFocusPainted(false);
         CustomSimulationButton.setPreferredSize(new Dimension(250, 50));
@@ -52,7 +68,7 @@ public class SimulationOptions extends JFrame {
 
         //simulation preset title
         JLabel simTitle = new JLabel("Simulation Presets");
-        simTitle.setFont(new Font("Monospaced", Font.ITALIC, 20));
+        simTitle.setFont(new Font("Monospaced", Font.BOLD, 20));
         simTitle.setBorder(BorderFactory.createLineBorder(Color.WHITE, 0));
         simTitle.setSize(250, 50);
         simTitle.setAlignmentX(JLabel.CENTER_ALIGNMENT);
@@ -63,7 +79,7 @@ public class SimulationOptions extends JFrame {
         JButton NormalDayButton = new JButton("Normal Day at ODU");
         NormalDayButton.setFont(new Font("Monospaced", Font.BOLD, 16));
         NormalDayButton.setForeground(Color.BLACK);
-        NormalDayButton.setBackground(new Color(98, 139, 145, 250));
+        NormalDayButton.setBackground(new Color(164, 196, 201, 250));
         NormalDayButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
         NormalDayButton.setFocusPainted(false);
         NormalDayButton.setPreferredSize(new Dimension(250, 50));
@@ -74,7 +90,7 @@ public class SimulationOptions extends JFrame {
         JButton ElkFireButton = new JButton("Fire in Elkhorn Garage");
         ElkFireButton.setFont(new Font("Monospaced", Font.BOLD, 16));
         ElkFireButton.setForeground(Color.BLACK);
-        ElkFireButton.setBackground(new Color(98, 139, 145, 250));
+        ElkFireButton.setBackground(new Color(164, 196, 201, 250));
         ElkFireButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
         ElkFireButton.setFocusPainted(false);
         ElkFireButton.setPreferredSize(new Dimension(250, 50));
@@ -85,7 +101,7 @@ public class SimulationOptions extends JFrame {
         JButton FootballButton = new JButton("Football Game");
         FootballButton.setFont(new Font("Monospaced", Font.BOLD, 16));
         FootballButton.setForeground(Color.BLACK);
-        FootballButton.setBackground(new Color(98, 139, 145, 250));
+        FootballButton.setBackground(new Color(164, 196, 201, 250));
         FootballButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
         FootballButton.setFocusPainted(false);
         FootballButton.setPreferredSize(new Dimension(250, 50));
@@ -95,7 +111,7 @@ public class SimulationOptions extends JFrame {
         JButton HomeButton = new JButton("Home");
         HomeButton.setFont(new Font("Monospaced", Font.BOLD, 16));
         HomeButton.setForeground(Color.BLACK);
-        HomeButton.setBackground(new Color(114, 145, 98, 250));
+        HomeButton.setBackground(new Color(178, 225, 153, 250));
         HomeButton.setBorder(BorderFactory.createLineBorder(Color.GRAY, 3));
         HomeButton.setFocusPainted(false);
         HomeButton.setPreferredSize(new Dimension(250, 50));
