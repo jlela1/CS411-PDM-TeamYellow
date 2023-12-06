81ae35cdf8a7fb20dd036ad54659b2c57aa59cd6
parth patel <92473672+ppate010@users.noreply.github.com>
2023-12-06 17:41:08 -0500

    userAnalysisGUI

---
+++ src/frontend/UserAnalysisGUI.java
@@ -0,0 +1,159 @@
+package frontend;
+
+import com.github.lgooddatepicker.components.DatePicker;
+import com.github.lgooddatepicker.components.DatePickerSettings;
+
+import javax.swing.*;
+import java.awt.*;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.WeekFields;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Random;
+
+public class UserAnalysisGUI extends JFrame {
+    private JLabel dailyLabel, weeklyLabel, monthlyLabel, totalLabel, recommendedLabel, reviewsLabel;
+    private DatePicker datePicker;
+
+    public UserAnalysisGUI() {
+        setTitle("User Analysis");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+
+        getContentPane().setLayout(new BorderLayout());
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.insets = new Insets(10, 10, 10, 10);
+
+        totalLabel = createLabel("Total Users: 0");
+        recommendedLabel = createLabel("Recommended Users: 0");
+        reviewsLabel = createLabel("Reviews: 0");
+
+        dailyLabel = createLabel("Daily Users: 0");
+        weeklyLabel = createLabel("Weekly Users: 0");
+        monthlyLabel = createLabel("Monthly Users: 0");
+
+        panel.add(totalLabel, gbc);
+        panel.add(recommendedLabel, gbc);
+        panel.add(reviewsLabel, gbc);
+
+        panel.add(createSeparator(), gbc);
+
+        panel.add(createLabel("Select Date:"), gbc);
+        datePicker = createDatePicker();
+        panel.add(datePicker, gbc);
+
+        panel.add(createSeparator(), gbc);
+
+        panel.add(dailyLabel, gbc);
+        panel.add(weeklyLabel, gbc);
+        panel.add(monthlyLabel, gbc);
+
+        add(panel);
+
+        updateStatistics();
+
+        setVisible(true);
+    }
+
+    private JLabel createLabel(String text) {
+        JLabel label = new JLabel(text);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setFont(new Font("Arial", Font.BOLD, 16));
+      //  label.setForeground(new Color(50, 50, 50));
+        label.setOpaque(true);
+      //  label.setBackground(new Color(220, 220, 220));
+        label.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+        return label;
+    }
+
+    private JSeparator createSeparator() {
+        JSeparator separator = new JSeparator(JSeparator.HORIZONTAL);
+      //  separator.setForeground(new Color(150, 150, 150));
+        return separator;
+    }
+
+    private DatePicker createDatePicker() {
+        DatePickerSettings dateSettings = new DatePickerSettings();
+        dateSettings.setFormatForDatesCommonEra("yyyy-MM-dd");
+        DatePicker datePicker = new DatePicker(dateSettings);
+        datePicker.setDateToToday();
+
+        datePicker.addDateChangeListener(event -> updateStatistics());
+
+        return datePicker;
+    }
+
+    private Container createContentPane() {
+        JPanel contentPane = new JPanel(new BorderLayout());
+
+        JPanel backgroundPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                ImageIcon backgroundImage = new ImageIcon("resources/Background 11.png");
+                g.drawImage(backgroundImage.getImage(), 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        backgroundPanel.setOpaque(false);
+        backgroundPanel.setLayout(new BorderLayout());
+
+        // Create a PDM footer
+        JPanel footer = PDMPanels.createFooter();
+        footer.setOpaque(false);
+        backgroundPanel.add(footer, BorderLayout.SOUTH);
+
+        contentPane.add(backgroundPanel, BorderLayout.CENTER);
+
+        return contentPane;
+    }
+
+    private void updateStatistics() {
+        int totalUsers = 15000;
+        int recommendedUsers = 5000;
+        int reviewsCount = 200;
+
+        totalLabel.setText("Total Users: " + totalUsers);
+        recommendedLabel.setText("Recommended Users: " + recommendedUsers);
+        reviewsLabel.setText("Reviews: " + reviewsCount);
+
+        int selectedWeek = datePicker.getDate().get(WeekFields.of(Locale.getDefault()).weekOfWeekBasedYear());
+        long seed = selectedWeek * 1000L;
+        Random random = new Random(seed);
+
+        int weeklyUsers = random.nextInt(1500);
+        weeklyLabel.setText("Weekly Users: " + weeklyUsers);
+
+        Map<DayOfWeek, Integer> dailyUsersMap = new HashMap<>();
+        int remainingDailyUsers = weeklyUsers;
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            int dailyValue = remainingDailyUsers > 0 ? random.nextInt(remainingDailyUsers) : 0;
+            dailyUsersMap.put(dayOfWeek, dailyValue);
+            remainingDailyUsers -= dailyValue;
+        }
+
+        int dailyUsers = dailyUsersMap.get(datePicker.getDate().getDayOfWeek());
+        dailyLabel.setText("Daily Users: " + dailyUsers);
+
+        LocalDate currentDate = datePicker.getDate();
+        int currentMonth = currentDate.getMonthValue();
+
+        long monthlySeed = currentMonth * 1000L;
+        Random monthlyRandom = new Random(monthlySeed);
+        int monthlyUsers = monthlyRandom.nextInt(5000);
+        monthlyLabel.setText("Monthly Users: " + monthlyUsers);
+
+        String formattedDate = currentDate.format(DateTimeFormatter.ofPattern("EEE, d MMM yyyy"));
+        setTitle("User Analysis - " + formattedDate);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new UserAnalysisGUI());
+    }
+}
